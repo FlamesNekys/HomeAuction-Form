fa3aed1374adbbb445b0adfa7bb390df056751e5
FlamesNekkys <yodacj21@gmail.com>
2025-01-27 22:56:14 +0300

    made entire form page

--- src/pages/FormPage.tsx
+++ src/pages/FormPage.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react';
 import { Formik } from 'formik';
 import FieldWrapper from '../components/FieldWrapper';
-import { Card } from '@chakra-ui/react';
+import { Card, VStack, StackSeparator, Button } from '@chakra-ui/react';
 
 const FormPage: FC = () => {
     return (
@@ -10,12 +10,33 @@ const FormPage: FC = () => {
                 <Formik
                     initialValues={{
                         name: '',
+                        address: '',
+                        floor: 0,
+                        totalFloors: 0,
+                        square: 0,
+                        livingSquare: 0,
+                        kitchenSquare: 0,
                     }}
                     onSubmit={(values) => {
                         console.log('Form submitted!', values);
                     }}
                 >
-                    <FieldWrapper name="name" label="Название объекта" type="text" />
+                    {(props) => (
+                        <form onSubmit={props.handleSubmit} className="form">
+                            <VStack separator={<StackSeparator />}>
+                                <FieldWrapper name="name" label="Название объекта" type="text" />
+                                <FieldWrapper name="address" label="Адрес" type="text" />
+                                <FieldWrapper name="floor" label="Этаж" type="number" />
+                                <FieldWrapper name="totalFloors" label="Количество этажей в доме" type="number" />
+                                <FieldWrapper name="square" label="Площадь" type="number" />
+                                <FieldWrapper name="livingSquare" label="Жилая площадь" type="number" />
+                                <FieldWrapper name="kitchenSquare" label="Площадь кухни" type="number" />
+                            </VStack>
+                            <Button mt={4} colorScheme="teal" type="submit" justifySelf="center">
+                                Отправить
+                            </Button>
+                        </form>
+                    )}
                 </Formik>
             </Card.Body>
         </Card.Root>
